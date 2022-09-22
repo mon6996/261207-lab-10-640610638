5607c89de38b50acec0ab7df5eb124545f9ba867
M <thanaporn_chan@cmu.ac.th>
2022-09-22 13:52:18 +0700

    completed

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  return res.json({ name: "Chayanin Suatap", studentId: "123456789" });
+export default function handler(req, res)
+{
+	return res.json({ name: "Thanaporn Chanchanayothin", studentId: "640610638" });
 }

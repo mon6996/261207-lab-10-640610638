10bf45ee1d60f53780bdc77d8a09adfd6eba6513
M <thanaporn_chan@cmu.ac.th>
2022-09-22 15:15:32 +0700

    completed

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -1,16 +1,36 @@
 import { readDB, writeDB } from "../../../../backendLibs/dbLib";
 import { v4 as uuidv4 } from "uuid";
 
-export default function roomIdMessageRoute(req, res) {
-  if (req.method === "GET") {
-    const rooms = readDB();
-  } else if (req.method === "POST") {
-    const rooms = readDB();
+export default function roomIdMessageRoute(req, res)
+{
+	if (req.method === "GET")
+	{
+		const rooms = readDB();
+		const id = req.query.roomId;
+		const roomIdx = rooms.findIndex((x) => x.roomId === id);
+		if (roomIdx ===  -1)
+			return res.status(404).json({ ok: false, message: "Invalid room id" });
+		return res.json({ ok: true, messages: rooms[roomIdx].messages });
+	}
+	else if (req.method === "POST")
+	{
+		const rooms = readDB();
+		const id = req.query.roomId;
+		const roomIdx = rooms.findIndex((x) => x.roomId === id);
+		if (roomIdx === -1)
+			return res.status(404).json({ ok: false, message: "Invalid room id" });
 
-    //read request body
-    const text = req.body.text;
-
-    //create new id
-    const newId = uuidv4();
-  }
+		if (typeof req.body.text !== "string" || req.body.text.length === 0)
+			return res.status(404).json({ ok: false, message: "Invalid text input" });
+		
+		const text = req.body.text;
+		const newId = uuidv4();
+		const newMessage = {
+			messageId: uuidv4(),
+			text: req.body.text,
+		};
+		rooms[roomIdx].messages.push(newMessage);
+		writeDB(rooms);
+		return res.json({ ok: true, message: newMessage});
+	}
 }

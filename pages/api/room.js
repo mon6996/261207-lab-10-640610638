4d9b7ca44c7bee0d5de2e296ce639e506f060f70
M <thanaporn_chan@cmu.ac.th>
2022-09-22 14:07:55 +0700

    completed

--- pages/api/room.js
+++ pages/api/room.js
@@ -1,5 +1,16 @@
 import { readDB } from "../../backendLibs/dbLib";
 
-export default function roomRoute(req, res) {
-  const rooms = readDB();
+export default function roomRoute(req, res)
+{
+	const rooms = readDB();
+
+	const data = rooms.map(x => ({
+		roomId: x.roomId,
+		roomName: x.roomName,
+	}))
+
+	return res.json({
+		ok: true,
+		rooms: data,
+	});
 }

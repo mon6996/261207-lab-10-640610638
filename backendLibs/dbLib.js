74f27c640286a005e96b0478395577b3630be1c2
M <thanaporn_chan@cmu.ac.th>
2022-09-22 20:48:54 +0700

    edit

--- backendLibs/dbLib.js
+++ backendLibs/dbLib.js
@@ -1,12 +1,12 @@
 import fs from 'fs';
 
 export function readDB() {
-  const str = fs.readFileSync("db/chatroom.json", { encoding: "utf-8" });
+  const str = fs.readFileSync("backendLibs/chatroom.json", { encoding: "utf-8" });
   const chatroom = JSON.parse(str);
   return chatroom;
 }
 
 export function writeDB(chatroom) {
   const str = JSON.stringify(chatroom);
-  fs.writeFileSync("db/chatroom.json", str, { encoding: "utf-8" });
+  fs.writeFileSync("backendLibs/chatroom.json", str, { encoding: "utf-8" });
 }
